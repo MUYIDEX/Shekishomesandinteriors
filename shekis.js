9b8c662983361f45c96fd9a57fb49a21d202752b
MUYIDEX <104993443+MUYIDEX@users.noreply.github.com>
2022-06-09 03:30:18 -0700

    Add files via upload

---
+++ shekis.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('.fa-bars').click(function(){
+   $(this).toggleClass('fa-times');
+   $('nav').toggleClass('nav-toggle');
+  });
+
+  $(window).on('scroll load',function(){
+    $('.fa-bars').removeClass('fa-times');
+    $('nav').removeClass('nav-toggle');
+  });
+
+  $('.projects').magnificPopup({
+    delegate:'a',
+    type:'image',
+    gallery:{
+      enabled:true
+    }
+    
+    })
+});
+
